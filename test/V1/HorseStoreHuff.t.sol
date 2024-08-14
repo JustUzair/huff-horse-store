6876ef65fbf9fdd7ab3800686833ecfefd4b19d8
JustUzair <uzairhajra763390@gmail.com>
2024-08-14 23:49:03 +0530

    added differential tests

--- test/V1/HorseStoreHuff.t.sol
+++ test/V1/HorseStoreHuff.t.sol
@@ -4,6 +4,9 @@ pragma solidity ^0.8.20;
 // import {Test} from "forge-std/Test.sol";
 import {Base_TestV1, HorseStore} from "./Base_TestV1.t.sol";
 import {HuffDeployer} from "foundry-huff/HuffDeployer.sol";
+import {HuffConfig} from "foundry-huff/HuffConfig.sol";
+
+// import "../../src/Horse-Store-V1/HorseStore.huff";
 
 contract HorseStoreHuff is Base_TestV1 {
     // HUffDeployer, checks src folder by default, also doesn't need .huff extension
